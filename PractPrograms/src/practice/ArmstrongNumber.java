4a3915d7b5d808aeb2e6544b0b58a51d2268747c
Yogesh Shinde <ysshinde450@gmail.com>
2022-10-06 23:38:54 +0530

    //comment added in ArmstrongNumber

--- PractPrograms/src/practice/ArmstrongNumber.java
+++ PractPrograms/src/practice/ArmstrongNumber.java
@@ -3,7 +3,7 @@ package practice;
 import java.util.Scanner;
 
 public class ArmstrongNumber {
-
+	//Git bash changed
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a=scan.nextInt();
